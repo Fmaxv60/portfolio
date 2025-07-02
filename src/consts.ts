396bd187e5c0363614b5415aecf10b8cec2d1036
Maxime Violette <maxime.violette@valeo.com>
2025-07-02 16:29:34 +0200

    Several additions and changes

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Maxime Violette';
-export const SITE_DESCRIPTION = 'Welcome to my portfolio!';
+export const SITE_DESCRIPTION = 'Bienvenue sur mon portfolio ! N\'hésite pas à me contacter pour plus d\'informations.';
--- src/content/blog/homelab.md
+++ src/content/blog/homelab.md
@@ -2,7 +2,7 @@
 title: 'Mon homelab'
 description: 'Voici mon architecture maison'
 pubDate: 'Jul 1 2025'
-heroImage: '../../assets/blog-placeholder-2.jpg'
+heroImage: '../../assets/homelab.jpg'
 ---
 
 ## Mon homelab
