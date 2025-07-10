e6c9c265facf848b5d81fe5b40334cbd90a27b83
Maxime V <maxime.v60@orange.fr>
2025-07-10 14:25:14 +0200

    Update SITE_DESCRIPTION

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Maxime Violette';
-export const SITE_DESCRIPTION = 'Bienvenue sur mon portfolio ! N\'hésite pas à me contacter pour plus d\'informations.';
+export const SITE_DESCRIPTION = 'Bienvenue sur mon portfolio ! Vient en apprendre plus sur moi et pourquoi pas collaborer avec moi !';
